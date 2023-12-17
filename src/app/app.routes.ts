b6f9f076b42a33b7cdd5bfdda915f565635e208f
Nathaniel Brian <nbmbestre91@gmail.com>
2023-12-17 22:15:47 +0800

    Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/haven', pathMatch: 'full'},
+    {path: 'haven', title: 'haven', component: LandingPageComponent},
+    {path: 'registration', title: 'Registration', component: RegistrationComponent},
+    {path: 'welcome', title: 'Welcome', component: WelcomeComponent},
+    {path: 'welcome/:id', title: 'Welcome', component: WelcomeComponent}
+];
