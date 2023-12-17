30f8b92c8dd8a3c63ec230762295cdae8e9b3aca
Nathaniel Brian <nbmbestre91@gmail.com>
2023-12-17 22:15:32 +0800

    App config for additional providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { HttpClientInMemoryWebApiModule, InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [
+    provideRouter(routes), 
+    provideAnimations(), 
+    provideHttpClient(),
+    importProvidersFrom(
+    [
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService)
+    ]
+    )
+  ],
 };
